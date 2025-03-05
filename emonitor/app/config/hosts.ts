d4553850c6369224209329ed4cdd6aa56ec2d257
Mihnea_Moso <mihnea.mosorescu@gmail.com>
2025-03-05 22:49:39 +0200

    edited hosts

--- emonitor/app/config/hosts.ts
+++ emonitor/app/config/hosts.ts
@@ -1,3 +1,3 @@
 export const devHost = "http://192.168.0.101:8000";
 export const prodHostRender = "https://emonitor-cwnf.onrender.com/";
-export const prodHost = "https://646f-188-27-154-164.ngrok-free.app";
+export const prodHost = "https://monkfish-ultimate-daily.ngrok-free.app";
